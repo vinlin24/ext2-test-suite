099d5d624590f6e7c01f0d70cf4faea5459eb392
Vincent Lin <vinlin24@outlook.com>
2023-03-06 18:45:29 -0800

    Fix erroor detection in setup.sh by using trap

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 
 if [ $# -eq 0 ]; then
     echo >&2 "$0: Specify the directory to set up in."
+    echo >&2 "USAGE: $0 /path/to/lab4"
     exit 1
 fi
 
@@ -26,24 +27,28 @@ if grep -q '# Test suite' "${lab_dir}/.gitignore" 2>/dev/null; then
     ignore_additions=""
 fi
 
-# Actual setup sequence.
-make suite &&
-    cp "$tarball_name" "$lab_dir" &&
-    cd "$lab_dir" &&
-    tar -xf "$tarball_name" &&
-    rm "$tarball_name" &&
-    echo -en "$ignore_additions" >>.gitignore
+# A trick to seeing if any command in a sequence exited with error.
+err=0
+trap 'err=1' ERR
 
-setup_code=$?
+# Actual setup sequence.
+make suite
+cp "$tarball_name" "$lab_dir"
+cd "$lab_dir"
+tar -xf "$tarball_name"
+rm "$tarball_name"
+echo -en "$ignore_additions" >>.gitignore
+
+# Check if any of the commands exited with error.
 
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 END=$(tput sgr0)
 
-if [ $setup_code -eq 0 ]; then
+if [ $err -eq 0 ]; then
     echo -n "${GREEN}The lab suite has been set up in ${lab_dir}. "
     echo "Happy coding!${END}"
 else
-    echo >&2 "${RED}Something went wrong...${END}"
+    echo >&2 "${RED}Something went wrong, setup might be incomplete.${END}"
     exit 1
 fi
